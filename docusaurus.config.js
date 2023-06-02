d33c98a9a37e4f5666780ba5dce55ae9f4bc4dd2
cassiov <cassioovs.@gmail.com>
2023-06-02 12:49:42 -0300

    Updated manifest.json for mode activation pt2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,12 +15,12 @@ const config = {
           {
             tagName: 'link',
             rel: 'icon',
-            href: './static/img/logopwa.png',
+            href: '/static/img/logopwa.png',
           },
           {
             tagName: 'link',
             rel: 'manifest',
-            href: './static/manifest.json',
+            href: '/static/manifest.json',
           },
           {
             tagName: 'meta',
@@ -40,18 +40,18 @@ const config = {
           {
             tagName: 'link',
             rel: 'apple-touch-icon',
-            href: './static/img/logopwa.png',
+            href: '/static/img/logopwa.png',
           },
           {
             tagName: 'link',
             rel: 'mask-icon',
-            href: './static/img/logopwa.png',
+            href: '/static/img/logopwa.png',
             color: 'rgb(37, 194, 160)',
           },
           {
             tagName: 'meta',
             name: 'msapplication-TileImage',
-            content: './static/img/logopwa.png',
+            content: '/static/img/logopwa.png',
           },
           {
             tagName: 'meta',
