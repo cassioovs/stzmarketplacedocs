dee57f1206c56a5627e0d5a093e2bd801926d066
cassiov <cassioovs.@gmail.com>
2023-06-02 22:26:47 -0300

    Ignore broken links fix to build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -89,7 +89,7 @@ const config = {
   projectName: 'stzmarketplacedocs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'log',
+  onBrokenMarkdownLinks: 'ignore',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
